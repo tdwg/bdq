d00f4dd4a113d1a3630e9635d734017170a24f5f
chicoreus <mole@morris.net>
2024-09-30 11:09:02 -0400

    Changing debugging output of sparql queries.

--- tg2/_review/build/draft_build-termlist_bdqcore.py
+++ tg2/_review/build/draft_build-termlist_bdqcore.py
@@ -398,8 +398,8 @@ for term in termLists:
 
             sparql = prefixes + "SELECT ?method ?predicate ?label ?specification ?specificationLabel  WHERE {  ?specification rdfs:label ?specificationLabel .  ?method bdqffdq:hasSpecification ?specification .  ?method ?predicate ?label .  ?method  bdqffdq:for"+testType+" bdqcore:"+row['term_localName']+" .  FILTER ( ?predicate = rdfs:label ) }"
             queryResult = graph.query(sparql)
+            print(sparql)
             for r in queryResult : 
-                print(sparql)
                 text += '\t\t<tr>\n'
                 text += '\t\t\t<td>bdqffdq:'+testType+'Method</td>\n'
                 text += '\t\t\t<td>' + str(r['method']) + '</td>\n'
