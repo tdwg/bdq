8fcc0f17bb71a5073895079b00e191804f3d71f7
chicoreus <mole@morris.net>
2024-10-09 08:40:18 -0400

    Adding a function to generate just a list of authors to function_lib.py.  Adding documentation to build_term_key and gracefully handling the case of a missmatch in terms provided and expected.

--- tg2/_review/build/function_lib.py
+++ tg2/_review/build/function_lib.py
@@ -1,12 +1,24 @@
 # Shared functions reused across build scripts
 # Paul J. Morris 
 # 2024-10-07
-# This script contains a library of reusable functions to support scripts to build of documents and term lists for tdwg standards and draft standards
+# This script contains a library of reusable functions to support scripts to 
+# build of documents and term lists for tdwg standards and draft standards
 
 import rdflib     # run sparql queries on rdf 
 from rdflib import Graph
 
-# Function build_term_key builds a markdown table of terms and examples for each term used to describe terms in a term-list document
+# Function build_term_key builds a markdown table of terms and examples 
+# for each term used to describe terms in a term-list document.
+#
+# @param term_concept_dictionary a dictionary with keys for each term 
+#   and values each being a dictionary with with keys label, term, normative
+#   where the term is the fully qualified term name, and normative is a 
+#   string with values of 'true', 'false', or '' 
+# @param terms_sorted_by_locaiton a dictionary containing at least one 
+#   record with entries for each term in the term_concept_dictionary
+#   used to provide an example for each term.
+# @return a markdown table of label, term, and metadata for each term in 
+#   the term_concept_dictionary
 def build_term_key(term_concept_dictionary, terms_sorted_by_localname) :
     # prefixes for sparql queries
     prefixes = """
@@ -23,7 +35,7 @@ def build_term_key(term_concept_dictionary, terms_sorted_by_localname) :
     definitionTable = definitionTable + "| ----- | ---- | ---------- | ------- | --------- |\n"
     termrow = terms_sorted_by_localname.iloc[0]
     for key, value in term_concept_dictionary.items() :
-        if value['label'] : 
+        if value['label'] and key in termrow.keys() : 
             label = value['label']
             termname = value['term']
             definition = ""
@@ -63,6 +75,7 @@ def build_term_key(term_concept_dictionary, terms_sorted_by_localname) :
 #  being rendered as separate (Authors\n: authors and Contributors\n: contributors) markdown definition list lists: 
 #
 # @see build_contributors_markdown(contributors_object)
+# @see build_authors_markdown(contributors_object)
 def build_authors_contributors_markdown(contributors_object) : 
 
     # Build the Markdown for the authors and contributors lists
@@ -123,6 +136,7 @@ def build_authors_contributors_markdown(contributors_object) :
 #  standards documentation section, that is [contributor_literal](contributor_iri) ([affiliation](affiliation_uri)).
 # 
 # @see build_authors_contributors_markdown(contributors_object)
+# @see build_authors_markdown(contributors_object)
 def build_contributors_markdown(contributors_object) : 
 
     # Build the Markdown for the contributors list
@@ -140,6 +154,49 @@ def build_contributors_markdown(contributors_object) :
                 contributors += '(' + contributor['affiliation'] + ')'
         separator = ", "
     return contributors    
+
+# Function build_authors_markdown builds a markdown list of authors excluding
+# contributors, where authors are identified by contributor_role=author, from 
+# an object expected to be loaded from an authors_configuration.yaml file.
+# a template file used with this function would be expected to contain the lines (
+# in the form of a markdown definition list): 
+# Authors
+# : {authors}  
+# preceeded and followed by blank lines 
+# and code using the output of this function would be expected to 
+# be used by a builder script that does a replacement in the form
+# authors = build_authors_markdown(contributors_yaml)
+# header = header.replace('{authors}', contributors)
+#
+# @param contributors_object an object with properties contributor_iri, contributor_literal, affiliation, affilitation_uri,
+#  contributor_role, role_uri where the value of contributor_role is used to determine inclusion, empty values will result in
+#  elements being omitted from the markdown.  
+#
+# @return a text string representing the list of authors in the form specfied by section 3.2.3.1 of the
+#  standards documentation section, that is [contributor_literal](contributor_iri) ([affiliation](affiliation_uri))
+#  limited to entries in contributor_yaml where the contributor_role = author.
+# 
+# @see build_authors_contributors_markdown(contributors_object)
+# @see build_contributors_markdown(contributors_object)
+def build_authors_markdown(contributors_object) : 
+
+    # Build the Markdown for the contributors list
+    contributors = ''
+    separator = ''
+    for contributor in contributors_object :
+        if contributor['contributor_role'] == 'author' : 
+            if contributor['contributor_iri'] :
+                contributors += separator + '[' + contributor['contributor_literal'] + '](' + contributor['contributor_iri'] + ') '
+            else : 
+                contributors += separator + contributor['contributor_literal'] + ' '
+            if contributor['affiliation'] :
+                if contributor['affiliation_uri'] :
+                    contributors += '([' + contributor['affiliation'] + '](' + contributor['affiliation_uri'] + '))'
+                else :
+                    contributors += '(' + contributor['affiliation'] + ')'
+            separator = ", "
+    return contributors    
+
 # ---------------
 # Function definitions (drawn from dwc build)
 # Steve Baskauf 2020-08-12 CC0
