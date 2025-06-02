3ad7c3bc596ef06f1546fbbdedecc7b9f70e42a3
chicoreus <mole@morris.net>
2025-06-02 14:46:43 -0400

    Changing build script to generate RDF representations of tests that include both the single record and multi record tests, regenerating the RDF representations.

--- tg2/_make_review/copy_files.sh
+++ tg2/_make_review/copy_files.sh
@@ -16,10 +16,10 @@ cp ../core/TG2_tests.csv ../_review/vocabulary/bdqtest_term_versions.csv
 # draft_build_bdqtest_singlerecord_tests_current.py
 cp ../core/TG2_tests.csv ../_review/dist/bdqtest_singlerecord_tests_current.csv
 
-# TODO: Replace these steps with creation of these files with kurator-ffdq
-cp ../core/TG2_tests.xml ../_review/dist/bdqtest.xml
-cp ../core/TG2_tests.ttl ../_review/dist/bdqtest.ttl
-cp ../core/TG2_tests.json ../_review/dist/bdqtest.json
+# Replace these steps with creation of these files with kurator-ffdq below
+# cp ../core/TG2_tests.xml ../_review/dist/bdqtest.xml
+# cp ../core/TG2_tests.ttl ../_review/dist/bdqtest.ttl
+# cp ../core/TG2_tests.json ../_review/dist/bdqtest.json
 
 # TODO: The above term-versions file will become the master copy, and include the multi-record measures.
 # append multi-record measures to csv list of core tests
@@ -39,11 +39,14 @@ cd ../../../
 # moved down into kurator-ffdq/ directory
 cd kurator-ffdq/
 # copy tests into the kurator-ffdq data directory from the bdq project
-grep -v "AllAmendmentTestsRunOnSingleRecord" ../bdq/tg2/core/TG2_tests.csv  | grep -v "AllDarwin" > data/TG2_tests.csv
-cp ../bdq/tg2/core/TG2_multirecord_measure_tests.csv data/TG2_multirecord_measure_tests.csv
+grep -v "AllAmendmentTestsRunOnSingleRecord" ../bdq/tg2/_review/vocabulary/bdqtest_term_versions.csv  | grep -v "AllDarwin" > data/TG2_tests.csv
+# to build rdf files with just the single record tests, copy just those into the kurator-ffdq data directory
+#grep -v "AllAmendmentTestsRunOnSingleRecord" ../bdq/tg2/core/TG2_tests.csv  | grep -v "AllDarwin" > data/TG2_tests.csv
 # run test-util.sh to generate various RDF serializations of the tests.
-./test-util.sh -config data/tg2_tests.properties -format RDFXML -out ../bdq/tg2/core/TG2_tests.xml -in  data/TG2_tests.csv -guidFile ../bdq/tg2/core/TG2_tests_additional_guids.csv -useCaseFile ../bdq/tg2/core/usecase_test_list.csv -ieGuidFile ../bdq/tg2/core/information_element_guids.csv
-./test-util.sh -config data/tg2_tests.properties -format TURTLE -out ../bdq/tg2/core/TG2_tests.ttl -in  data/TG2_tests.csv -guidFile ../bdq/tg2/core/TG2_tests_additional_guids.csv -useCaseFile ../bdq/tg2/core/usecase_test_list.csv -ieGuidFile ../bdq/tg2/core/information_element_guids.csv
-./test-util.sh -config data/tg2_tests.properties -format JSON-LD -out ../bdq/tg2/core/TG2_tests.json -in  data/TG2_tests.csv -guidFile ../bdq/tg2/core/TG2_tests_additional_guids.csv -useCaseFile ../bdq/tg2/core/usecase_test_list.csv -ieGuidFile ../bdq/tg2/core/information_element_guids.csv
-./test-util.sh -config data/tg2_tests.properties -format RDFXML -out ../bdq/tg2/core/TG2_multirecord_measure_tests.xml -in  data/TG2_multirecord_measure_tests.csv -guidFile ../bdq/tg2/core/TG2_tests_additional_guids.csv -useCaseFile ../bdq/tg2/core/usecase_test_list.csv -ieGuidFile ../bdq/tg2/core/information_element_guids.csv
-./test-util.sh -config data/tg2_tests.properties -format TURTLE -out ../bdq/tg2/core/TG2_multirecord_measure_tests.ttl -in  data/TG2_multirecord_measure_tests.csv -guidFile ../bdq/tg2/core/TG2_tests_additional_guids.csv -useCaseFile ../bdq/tg2/core/usecase_test_list.csv -ieGuidFile ../bdq/tg2/core/information_element_guids.csv
+./test-util.sh -config data/tg2_tests.properties -format RDFXML -out ../bdq/tg2/_review/dist/bdqtest.xml -in  data/TG2_tests.csv -guidFile ../bdq/tg2/core/TG2_tests_additional_guids.csv -useCaseFile ../bdq/tg2/core/usecase_test_list.csv -ieGuidFile ../bdq/tg2/core/information_element_guids.csv
+./test-util.sh -config data/tg2_tests.properties -format TURTLE -out ../bdq/tg2/_review/dist/bdqtest.ttl -in  data/TG2_tests.csv -guidFile ../bdq/tg2/core/TG2_tests_additional_guids.csv -useCaseFile ../bdq/tg2/core/usecase_test_list.csv -ieGuidFile ../bdq/tg2/core/information_element_guids.csv
+./test-util.sh -config data/tg2_tests.properties -format JSON-LD -out ../bdq/tg2/_review/dist/bdqtest.json -in  data/TG2_tests.csv -guidFile ../bdq/tg2/core/TG2_tests_additional_guids.csv -useCaseFile ../bdq/tg2/core/usecase_test_list.csv -ieGuidFile ../bdq/tg2/core/information_element_guids.csv
+# to build separate RDF files for the multi-record measure tests copy the csv file with just those tests and execute kurator-ffdq on it
+#cp ../bdq/tg2/core/TG2_multirecord_measure_tests.csv data/TG2_multirecord_measure_tests.csv
+#./test-util.sh -config data/tg2_tests.properties -format RDFXML -out ../bdq/tg2/core/TG2_multirecord_measure_tests.xml -in  data/TG2_multirecord_measure_tests.csv -guidFile ../bdq/tg2/core/TG2_tests_additional_guids.csv -useCaseFile ../bdq/tg2/core/usecase_test_list.csv -ieGuidFile ../bdq/tg2/core/information_element_guids.csv
+#./test-util.sh -config data/tg2_tests.properties -format TURTLE -out ../bdq/tg2/core/TG2_multirecord_measure_tests.ttl -in  data/TG2_multirecord_measure_tests.csv -guidFile ../bdq/tg2/core/TG2_tests_additional_guids.csv -useCaseFile ../bdq/tg2/core/usecase_test_list.csv -ieGuidFile ../bdq/tg2/core/information_element_guids.csv
