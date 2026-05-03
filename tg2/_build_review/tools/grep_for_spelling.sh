784b3afe4f916d0e9464116528f0d124523d1c9a
chicoreus <mole@morris.net>
2026-05-03 19:36:53 -0400

    For #309 related to #314 adding a shell script that greps for the most common spelling errors and inconsistency issues found in standard documents.

---
+++ tg2/_build_review/tools/grep_for_spelling.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+grep -R -i --color=always --exclude-dir=tools "Amendement" ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools "Fittnes" ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools "captialized" ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools “Vaildation” ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools “INTERNAL_PREREQUISTITES_NOT_MET” ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools “INTERNAL_PREREQUISTES_NOT_MET” ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools Fittness ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools "BDQ Tests and Assertions" ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools "fittness" ../../_build_review/ 
+grep -R -i --color=always --exclude-dir=tools "precicely" ../../_build_review/ 
+grep -R -i --color=always --exclude-dir=tools "bdqq" ../../_build_review/ 
+grep -R -i --color=always --exclude-dir=tools "organisational" ../../_build_review/ 
+grep -R -i --color=always --exclude-dir=tools "non normative" ../../_build_review/templates/
+grep -R --color=always --exclude-dir=tools "bdq:" ../../_build_review/templates/
+grep -R -i --color=always --exclude-dir=tools "determin " ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools "covention" ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools "covention" ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools "implemenation" ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools "programing" ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools "mechansism" ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools "modelling" ../../_build_review/
+grep -R -i --color=always --exclude-dir=tools "fittness" ../../_build_review/
+
+grep -R -i --color=always --exclude-dir=tools "Amendement" ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools "Fittnes" ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools "captialized" ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools “Vaildation” ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools “INTERNAL_PREREQUISTITES_NOT_MET” ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools “INTERNAL_PREREQUISTES_NOT_MET” ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools Fittness ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools "BDQ Tests and Assertions" ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools "fittness" ../../_review/vocabulary/ 
+grep -R -i --color=always --exclude-dir=tools "precicely" ../../_review/vocabulary/ 
+grep -R -i --color=always --exclude-dir=tools "bdqq" ../../_review/vocabulary/ 
+grep -R -i --color=always --exclude-dir=tools "organisational" ../../_review/vocabulary/ 
+grep -R -i --color=always --exclude-dir=tools "non normative" ../../_review/vocabulary/
+grep -R --color=always --exclude-dir=tools "bdq:" ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools "determin " ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools "covention" ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools "covention" ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools "implemenation" ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools "programing" ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools "mechansism" ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools "modelling" ../../_review/vocabulary/
+grep -R -i --color=always --exclude-dir=tools "fittness" ../../_review/vocabulary/
